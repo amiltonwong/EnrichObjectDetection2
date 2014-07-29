64c249837841ceacaa81457f82c0a8e0c983cdc8
Christopher B. Choy <chrischoy208@gmail.com>
2014-07-29 16:21:45 -0700

    minor change

---
+++ demoCG_GPU_MCMC.m
@@ -0,0 +1,75 @@
+% Load image
+addpath('HoG');
+addpath('HoG/features');
+addpath('Util');
+addpath('../MatlabRenderer/');
+
+if ~exist('renderer','var')  
+  model_file = 'Mesh/Bicycle/road_bike';
+  renderer = Renderer();
+  if ~renderer.initialize([model_file '.3ds'], 700, 700, 0, 0, 0, 0, 25)
+    error('fail to load model');
+    renderer.delete();
+    clear renderer;
+  end
+end
+
+
+if ~exist('Gamma','var')
+  load('Statistics/sumGamma_N1_40_N2_40_sbin_4_nLevel_10_nImg_1601_napoli1_gamma.mat');
+end
+
+hog_cell_threshold = 1.5 * 10^0;
+padding = 20;
+n_cell_limits = [50 100 150 200 250 300 350 400 450 500];
+% n_cell_limits = 200;
+lambda = 0.01;
+
+cg_time_per_case = zeros(1,numel(n_cell_limits));
+cg_residual_per_case = zeros(1,numel(n_cell_limits));
+
+CG_THREASHOLD = 10^-3;
+CG_MAX_ITER = 60;
+N_THREAD = 32;
+
+%%%%%%%% Get HOG template
+
+% create white background padding
+paddedIm = padarray(im2double(im), [padding, padding, 0]);
+paddedIm(:,1:padding,:) = 1;
+paddedIm(:,end-padding+1 : end, :) = 1;
+paddedIm(1:padding,:,:) = 1;
+paddedIm(end-padding+1 : end, :, :) = 1;
+
+% bounding box coordinate x1, y1, x2, y2
+bbox = [1 1 size(im,2) size(im,1)] + padding;
+
+scrambleKernel = parallel.gpu.CUDAKernel('scrambleGammaToSigma.ptx','scrambleGammaToSigma.cu');
+scrambleKernel.ThreadBlockSize = [N_THREAD , N_THREAD , 1];
+
+GammaGPU = gpuArray(single(Gamma));
+gammaDim = size(Gamma);
+  
+for caseIdx = 1:numel(n_cell_limits)
+  n_cell_limit = n_cell_limits(caseIdx);
+  
+  tic;
+
+  renderer.setViewpoint(az,el,ismac*180 + yaw,0,fov);
+  [im, depth] = renderer.renderCrop();
+  [WHOTemplate_CG, ~, r_hist, residual] = WHOTemplateCG_GPU( im, scrambleKernel, mu, GammaGPU, gammaDim(1), n_cell_limit, lambda, padding, hog_cell_threshold, CG_THREASHOLD, CG_MAX_ITER, N_THREAD);
+  
+  cg_time_per_case(caseIdx) = toc
+
+  cg_residual_per_case(caseIdx) = residual
+end
+
+figure(1); imagesc(HOGpicture(abs(WHOTemplate_CG))); colorbar; title('Conjugate Gradient nonzero cells');
+
+figure(2); plot(n_cell_limits, cg_time_per_case,'-+'); title('Conjugate Gradient/Decomposition nonzero cells'); xlabel('N Cells'); ylabel('seconds');
+legend('CG');
+
+figure(3); plot(n_cell_limits, cg_residual_per_case, '-+'); title('Conjugate Gradient/Decomposition nonzero cells'); xlabel('N Cells'); ylabel('residuals');
+legend('CG');
+
+figure(4); semilogy(r_hist)
---
+++ dwot_make_detectors_slow_gpu.m
@@ -0,0 +1,87 @@
+function [detectors, detector_name]= dwot_make_detectors_slow_gpu(Mu, Gamma, mesh_path, azs, els, yaws, fovs, n_cell_limit, lambda, visualize)
+
+mesh_name = strrep(mesh_path, '/', '_');
+
+detector_name = sprintf('%s_lim_%d_lam_%0.4f_a_%d_e_%d_y_%d_f_%d.mat',...
+    mesh_name, n_cell_limit, lambda, numel(azs), numel(els), numel(yaws), numel(fovs));
+
+  
+if exist(detector_name,'file')
+  load(detector_name);
+else
+  if exist('renderer','var')
+    renderer.delete();
+    clear renderer;
+  end
+  
+  CG_THREASHOLD = 10^-4;
+  CG_MAX_ITER = 60;
+  N_THREAD = 32;
+  
+  hog_cell_threshold = 1.5;
+  padding = 50;
+  
+  if ~exist('scrambleGammaToSigma.ptx','file')
+    system('nvcc -ptx scrambleGammaToSigma.cu');
+  end
+  scrambleKernel = parallel.gpu.CUDAKernel('scrambleGammaToSigma.ptx','scrambleGammaToSigma.cu');
+  scrambleKernel.ThreadBlockSize = [N_THREAD , N_THREAD , 1];
+
+  GammaGPU = gpuArray(single(Gamma));
+  gammaDim = size(Gamma);
+
+  
+  renderer = Renderer();
+  if ~renderer.initialize([mesh_path], 700, 700, 0, 0, 0, 0, 25)
+    error('fail to load model');
+  end
+  
+  i = 1;
+  detectors = cell(1,numel(azs) * numel(els) * numel(fovs));
+  try
+    for azIdx = 1:numel(azs)
+      for elIdx = 1:numel(els)
+        for yawIdx = 1:numel(yaws)
+          for fovIdx = 1:numel(fovs)
+            elGT = els(elIdx);
+            azGT = azs(azIdx);
+            yawGT = yaws(yawIdx);
+            fovGT = fovs(fovIdx);
+            tic
+            renderer.setViewpoint(90-azGT,elGT,yawGT,0,fovGT);
+            im = renderer.renderCrop();
+            % [ WHOTemplate, HOGTemplate] = WHOTemplateDecompNonEmptyCell( im, Mu, Gamma, n_cell_limit, lambda, 50);
+            % [ WHOTemplate, HOGTemplate] = WHOTemplateCG( im, Mu, Gamma, n_cell_limit, lambda, 50, 1.5, 10^-3, 100);
+            [WHOTemplate] = WHOTemplateCG_GPU( im, scrambleKernel, Mu, GammaGPU, gammaDim(1), n_cell_limit, lambda, padding, hog_cell_threshold, CG_THREASHOLD, CG_MAX_ITER, N_THREAD);
+            toc;
+
+            detectors{i}.whow = WHOTemplate;
+            % detectors{i}.hogw = HOGTemplate;
+            detectors{i}.az = azGT;
+            detectors{i}.el = elGT;
+            detectors{i}.yaw = yawGT;
+            detectors{i}.fov = fovGT;
+            detectors{i}.rendering = im;
+            detectors{i}.sz = size(WHOTemplate);
+            % detectors{i}.whopic = HOGpicture(WHOTemplate);
+
+            if visualize
+              figure(1); subplot(131);
+              imagesc(im); axis equal; axis tight;
+              subplot(132);
+              imagesc(HOGpicture(HOGTemplate)); axis equal; axis tight;
+              subplot(133);
+              imagesc(HOGpicture(WHOTemplate)); axis equal; axis tight;
+              disp('press any button to continue');
+              waitforbuttonpress;
+            end
+            i = i + 1;    
+          end
+        end
+      end
+    end
+  catch e
+    disp(e.message);
+  end
+  renderer.delete();
+end
