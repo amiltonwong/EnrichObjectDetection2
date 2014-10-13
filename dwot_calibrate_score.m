11cb45d5d7bd2e7cc160264e386522cf5d8d3fa9
Christopher B. Choy <chrischoy208@gmail.com>
2014-10-13 15:58:15 -0700

    add dwot_calibrate_score file

---
+++ dwot_calibrate_score.m
@@ -0,0 +1,9 @@
+function calibrated_score = dwot_calibrate_score(score, detector_idx, detectors, param)
+switch param.calibration_mode
+    case 'gaussian'
+        calibrated_score = (score - detectors{detector_idx}.mean)/detectors{detector_idx}.var ;            
+    case 'linear'
+        calibrated_score = detectors{detector_idx}.a * score + detectors{detector_idx}.b;
+    otherwise
+        error(['Calibration mode undefined : ', param.calibration_mode]);
+end
